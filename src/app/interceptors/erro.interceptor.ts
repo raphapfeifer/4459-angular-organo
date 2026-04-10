a419f488b4156bb678fb11adba178e50feabbf0c
raphapfeifer <rapha_pfeifer@yahoo.com.br>
2026-04-10 14:46:09 -0300

    Error message config

--- src/app/interceptors/erro.interceptor.ts
+++ src/app/interceptors/erro.interceptor.ts
@@ -1,11 +1,16 @@
 import { HttpErrorResponse, HttpInterceptorFn } from '@angular/common/http';
 import { catchError, throwError } from 'rxjs';
+import { MensagemErroService } from '../services/mensagem-erro.service';
+import { inject } from '@angular/core';
 
 export const erroInterceptor: HttpInterceptorFn = (req, next) => {
+
+  const mensagemErroService = inject(MensagemErroService);
+
   return next(req).pipe(
     catchError((erro: HttpErrorResponse) => {
       const mensagemErro = 'Aconteceu um erro'
-      console.log(mensagemErro)
+      mensagemErroService.mostrarMensagemDeErro(mensagemErro);
       return throwError(() => erro)
     })
   )
